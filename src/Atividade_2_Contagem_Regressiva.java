27f7690b042e4a9bf731845a93258b396c6c3853
nivoningles <contadonivon@gmail.com>
2024-09-16 21:53:26 -0300

    Atualização

---
+++ src/Atividade_2_Contagem_Regressiva.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Atividade_2_Contagem_Regressiva {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        int numero = 0;
+
+        System.out.print("Informe o um número: ");
+        numero = sc.nextInt();
+
+        while (numero > -1){
+            System.out.println(numero);
+            numero = numero - 1;
+
+        }
+
+    }
+}
